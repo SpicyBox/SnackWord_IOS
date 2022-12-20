b5090fd5610ccc0a8b73747e9c0a9dfaae903779
Leejeongchan1733026 <cksqls288@gmail.com>
2022-12-20 10:00:35 +0900

    프로잭트 생성

---
+++ SnackWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnackWord
+//
+//  Created by 이정찬 on 2022/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
