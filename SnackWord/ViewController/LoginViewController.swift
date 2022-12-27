a9bf2c5ae5d4ac11f66e47ea172cc94b7f1dfbb9
Leejeongchan1733026 <cksqls288@gmail.com>
2022-12-27 22:27:13 +0900

    firebase로그인 셋팅

--- SnackWord/ViewController/LoginViewController.swift
+++ SnackWord/ViewController/LoginViewController.swift
@@ -6,15 +6,27 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 
 class loginViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var idTxtField: UITextField!
+    
+    func login(email: String, password: String){
+        Auth.auth().signIn(withEmail: email, password: password) {
+            [weak self] authResult, error in
+            guard let strongSelf = self else { return }
+        }
+    }
     
     override func viewDidLoad() {
+        FirebaseApp.configure()
         super.viewDidLoad()
-
-
+    
     }
     
 }
+
+
